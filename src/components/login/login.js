2c5e187bfa61fdfef776ad760cb7aaf4d857aa12
xiaobai <l>
2017-08-31 09:58:08 +0800

    登录页面登陆功能实现

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,14 +2,44 @@
  * Created by Luo on 2017/8/30.
  */
 angular.module('login',[])
-    .directive('nglLogin',[function(){
+    .directive('nglLogin',[
+        function(){
         return{
             scope:{},
             restrict:'E',
             templateUrl:'/src/components/login/login.html',
             replace:true,
-            controller:['$scope',function($scope){
-                console.log('登录页面');
-            }]
+            controller:'nglLoginCtr'
         }
-    }]);
+    }])
+    .controller('nglLoginCtr',[
+        '$scope',
+        '$http',
+        '$location',
+        function($scope,$http,$location){
+        //登录功能
+         $scope.user = {
+             tc_name:'',
+             tc_pass:''
+         },
+         $scope.login = function(){
+             $http({
+                 url:'/api/login',
+                 method:'post',
+                 data:'tc_name='+$scope.user.tc_name +'&'+'tc_pass='+$scope.user.tc_pass,
+                 headers:{
+                     'Content-Type':'application/x-www-form-urlencoded'
+                 }
+             })
+             .then(function(resp){
+                 //console.log(resp);
+                 if(resp.data.code == 200){
+                     localStorage.setItem('userInfo',JSON.stringify(resp.data.result));
+                     $location.path('/')
+                 }else{
+                     alert('服务器错误');
+                 }
+
+             })
+         }
+    }])
